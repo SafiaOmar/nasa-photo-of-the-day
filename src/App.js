1c3a51b6cffdf98bf5a2739de255929984ace254
SafiaOmar <90978408+SafiaOmar@users.noreply.github.com>
2021-11-04 00:39:59 -0400

    1st day

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import axios from 'axios'
+import Img from './img'
+import Explain from './Explain'
+import Extra from './Extra'
+
 
 function App() {
+
+  const [nasa, setNasa] = useState([])
+  useEffect(() => {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=VxmyeETx5uJSltRR3PiVxvyyOu4P0DfNlLkbupmJ`)
+    .then(res => {
+      setNasa(res.data)
+    })
+    .catch(error => {
+      console.error(error)
+    })
+  },[])
+  console.log(nasa)
+
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1>Nasa Image Of the Day</h1>
+      <Extra nasaExtra = {nasa}/>
+      <input/> <button>Reset</button>
+      <Img nasaImg = {nasa}/>
+      <Explain nasaInfo = {nasa}/>
     </div>
   );
 }
